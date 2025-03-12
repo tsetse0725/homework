06c20fe03ad4e2606d362356ff99788e05a8cddb
tsetse0725 <tse9406@gmail.com>
2025-03-12 10:23:27 +0800

    update hiisen

--- script.js
+++ script.js
@@ -16,8 +16,32 @@ redButton.addEventListener("click", () => {
   box.style.backgroundColor = "red";
 });
 
-randomButton.addEventListener("click", () => {
-  const colors = ["yellow", "blue", "red"];
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  box.style.backgroundColor = colors[randomIndex];
+// randomButton.addEventListener("click", () => {
+//   const colors = ["yellow", "blue", "red"];
+//   const randomIndex = Math.floor(Math.random() * colors.length);
+//   box.style.backgroundColor = colors[randomIndex];
+// });
+
+
+
+// randomButton.addEventListener("click", () => {
+// const r = Math.floor(Math.random() * 256);
+// const g = Math.floor(Math.random() * 256);
+// const b = Math.floor(Math.random() * 256);
+//   box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+//   console.log(r)
+// } );
+
+
+
+randomButton.addEventListener("click", () =>{
+  const hexNumbers ="0123456789ABCDEF";
+
+  let colorCode = "#";
+  for(let i = 0; i < 6; i++){
+    colorCode += hexNumbers[Math.floor(Math.random() * 17)];
+  box.style.backgroundColor = colorCode;
+  }
 });
+
+
